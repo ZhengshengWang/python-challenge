1678ceb395cf9ca33c87e22906c325da8b898569
Zhengsheng Wang <zsheng.wang99@gmail.com>
2024-07-18 22:53:05 -0500

    Finished

--- PyBank/main.py
+++ PyBank/main.py
@@ -5,29 +5,31 @@ import csv
 # Path to collect data from the Resources folder
 
 csvpath = os.path.join("Resources","budget_data.csv")
+# open and read csv
 with open (csvpath) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
     csv_header = next(csvfile)
+# skip header
     print(f"Header:{csv_header}")
-    
+#set variables  
     months=[]
     profit=[]
     changes=[]
-    
+#read through each row   
     for rows in csvreader:
         months.append(rows[0])    
         profit.append(int(rows[1]))
-
+#find change of profits and average
     for x in range(len(profit)-1):
         changes.append((int(profit[x+1])-int(profit[x])))
-    
+#Define each value needed
     avergae= round(sum(changes)/len(changes),2)
     total_months=len(months)
     greatest_increase= max(changes)
     greatest_decrease= min(changes)
     greatest_increase_month = changes.index(max(changes)) + 1
     greatest_decrease_month = changes.index(min(changes)) + 1
-
+#print result as required
     print("Financial Analysis")
     print("----------------------------")
     print("Total Months:" + str(total_months))
@@ -35,7 +37,7 @@ with open (csvpath) as csvfile:
     print("Average Change:" + "$" + str(avergae))
     print("Greatest Increase in Profits:" + str(months[greatest_increase_month]) + "($ " + str(greatest_increase) + ")")
     print("Greatest Decrease in Profits:" + str(months[greatest_decrease_month]) + "($ " + str(greatest_decrease) + ")")
-
+#open a new text file and write the result
     f = open("analysis/analysis.txt", "w")
     f.write("Financial Analysis\n")
     f.write("----------------------------\n")
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -5,30 +5,36 @@ import csv
 # Path to collect data from the Resources folder
 
 csvpath = os.path.join("Resources","election_data.csv")
+# open and read csv
 with open (csvpath) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
     csv_header = next(csvfile)
     print(f"Header:{csv_header}")
-
+#set each variables total= total vote candidatelist=list for all candidates unique_candidates=list for different candidates
+#vote= vote each candidates get vote percent= percent of each candidates
     total=0
     candidatelist = []
     unique_candidate = []
     vote=[]
     vote_percent=[]
-
+#read through rows get the total votes
     for rows in csvreader:
         total = total + 1
         candidatelist.append(rows[2])
-        
+#get a set from the candidatelist to get the unique candidate names       
     for x in set(candidatelist):
         unique_candidate.append(x)
+#y is total vote each candidate get       
         y = candidatelist.count(x)
         vote.append(y)
+#calculate the percent        
         p_count = round((y/total)*100,2)
         vote_percent.append(p_count)
-    
+#find the winner    
     win_vote = max(vote)
     winner = unique_candidate[vote.index(win_vote)]
+
+#print the result
     print("-------------------------")
     print("Election Results")   
     print("-------------------------")
@@ -40,15 +46,15 @@ with open (csvpath) as csvfile:
     print("The winner is: " + winner)
     print("-------------------------")
 
-
+#creat the text file with results
     f = open("analysis/Election Result.txt", "w")
     f.write("\n-------------------------")
     f.write("\nElection Results")   
     f.write("\n-------------------------")
     f.write("\nTotal Votes :" + str(total))    
-    f.write("\n-------------------------")
+    f.write("\n-------------------------\n")
     for i in range(len(unique_candidate)):
-            print(unique_candidate[i] + ": " + str(vote_percent[i]) +"% (" + str(vote[i])+ ")")
-    f.write("\n-------------------------")
+            f.write(unique_candidate[i] + ": " + str(vote_percent[i]) +"% (" + str(vote[i])+ ")\n")
+    f.write("-------------------------")
     f.write("\nThe winner is: " + winner)
     f.write("\n-------------------------")
